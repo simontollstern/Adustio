c7f3b15e4047aedbbe7a99fa509c93b0ea154533
Mmollle <moltas.bengtsson1@gmail.com>
2017-06-14 01:01:23 +0200

    Fixed names, added datetime row to Post database

--- Blog/models.py
+++ Blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
 import datetime
+
 # Create your models here.
 
 # Creates rows in the database where blog post data will be saved
-class BlogPosts(models.Model):
+class Post(models.Model):
     # The rows, more will be added later
-    post_title = models.CharField(max_length=150)
-    post_text = models.CharField(max_length=15000)
-    # post_pub_date = datetime.now()
+    title = models.CharField(max_length=150)
+    text = models.CharField(max_length=15000)
+    date = models.DateTimeField(default=datetime.datetime.now())
+
     def __str__(self):
-        return self.post_title
+        return self.title
--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
-from .models import BlogPosts
+from .models import Post
 
 # Create your views here.
 
 # Returns blog post data from the database as a string
 def index(request):
-    #all_blog_posts = BlogPosts.objects.all()
     return render(request, 'index.html', {
-        'posts': BlogPosts.objects.all()
+        'posts': Post.objects.all()
     })
 
+    #all_blog_posts = BlogPosts.objects.all()
     #return HttpResponse("<table><tr>" + p.post_title + " " + p.post_text + "</tr></table>" for p in all_blog_posts)
